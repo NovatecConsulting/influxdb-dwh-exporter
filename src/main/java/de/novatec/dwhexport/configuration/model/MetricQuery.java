b11a88ba80f4553ab5bd04308b3b1e5cfac8dad5
Jochen Just <jochen.just@novatec-gmbh.de>
2023-05-17 09:17:51 +0200

    Uses Spring Boot 3.0.x

--- src/main/java/de/novatec/dwhexport/configuration/model/MetricQuery.java
+++ src/main/java/de/novatec/dwhexport/configuration/model/MetricQuery.java
@@ -2,7 +2,7 @@ package de.novatec.dwhexport.configuration.model;
 
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotBlank;
 
 @Data
 public class MetricQuery {
